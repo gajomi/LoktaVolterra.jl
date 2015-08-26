99802c5cceeb868017f80328d178353ce82351d8
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-08-26 16:31:20 -0700

    factored out abstract functionality and GLV type

--- src/LotkaVolterra.jl
+++ src/LotkaVolterra.jl
@@ -1,5 +1,17 @@
 module LotkaVolterra
 
-# package code goes here
+using ODE
 
-end # module
+import Base.show
+
+export AbstractLotkaVolterraSystem,LotkaVolterraSystem,
+       rate,specificrate,jacobian,
+       fixedpoint,
+       isfeasible,isstable,feasibility,stability,viability,
+       odeint
+
+include("utils.jl")
+include("abstract.jl")
+include("general.jl")
+
+end
