1deecfa539478bd258c405c9d0c841e57d3a95d9
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-08-31 00:20:26 -0700

    enforce real on small matrix eigs test

--- src/utils.jl
+++ src/utils.jl
@@ -1,13 +1,13 @@
 function eigmaxreal(A::Matrix)
   if size(A,1)<= 2
-    return maximum(eigvals(A))
+    return maximum(real(eigvals(A)))
   else
     return eigs(A,nev=1,which = :LR)[1][1]
   end
 end
 function eigminreal(A::Matrix)
   if size(A,1)<= 2
-    return minimum(eigvals(A))
+    return minimum(real(eigvals(A)))
   else
     return eigs(A,nev=1,which = :SR)[1][1]
   end
