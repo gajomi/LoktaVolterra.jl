05dd6abcd535836b8735db3c02f0630ecd47e62a
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-11-03 01:01:45 -0800

    temoprary store of all data whilst deleting dotfiles

--- src/LotkaVolterra.jl
+++ src/LotkaVolterra.jl
@@ -7,19 +7,20 @@ using ODE
 import Base.show
 
 export AbstractLotkaVolterra,
-       communitymatrix,intrinsicrate,
-       issector,sectors,
-       LogisticGrowth,GeneralizedLotkaVolterra,
-       rate,specificrate,jacobian,
-       somefixedpoint,isfixedpoint,fixedpoint,fixedpoints,
-       somefeasible,isfeasible,feasibility,
-       somestable,isstable,stability,
-       someviable,isviable,viability,
-       odeint
+      LogisticGrowth,PredatorPrey,GeneralizedLotkaVolterra,
+      communitymatrix,intrinsicrate,
+      issector,sectors,
+      rate,specificrate,jacobian,
+      somefixedpoint,isfixedpoint,fixedpoint,fixedpoints,
+      somefeasible,isfeasible,feasibility,
+      somestable,isstable,stability,
+      someviable,isviable,viability,
+      odeint
 
 include("utils.jl")
 include("abstract.jl")
 include("logistic.jl")
+include("predatorprey.jl")
 include("general.jl")
 
 end
